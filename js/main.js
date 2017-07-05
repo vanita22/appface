a7f1bc7e8c4345a4b55d802d507404de75e5875c
vanita22 <inesvanessapcm@gmail.com>
2017-07-05 13:06:53 -0400

    subiendo nuevos codigos

--- js/main.js
+++ js/main.js
@@ -55,4 +55,12 @@ FB.Event.subscribe('xfbml.render', finished_rendering);
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=152471491968489";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 
