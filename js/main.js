0f1759cb093c8b2f46c5475e2e634ec92d0c107a
vanita22 <inesvanessapcm@gmail.com>
2017-07-05 13:13:49 -0400

    subiendo nuevos codigos de nuevo

--- js/main.js
+++ js/main.js
@@ -1,38 +1,3 @@
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '152471491968489',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();   
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-
-{
-    status: 'connected',
-    authResponse: {
-        accessToken: '...',
-        expiresIn:'...',
-        signedRequest:'...',
-        userID:'...'
-    }
-}
-
-
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
@@ -47,13 +12,7 @@ spinner.removeChild(spinner.childNodes[0]);
 }
 FB.Event.subscribe('xfbml.render', finished_rendering);
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=152471491968489";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+
 
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
