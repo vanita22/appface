33ab869cc99d440fde8d162d9e5557be236ed141
vanita22 <inesvanessapcm@gmail.com>
2017-07-05 13:16:34 -0400

    subiendo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function checkLoginState() {
+/*function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });
@@ -20,6 +20,6 @@ FB.Event.subscribe('xfbml.render', finished_rendering);
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=152471491968489";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));*/
 
 
